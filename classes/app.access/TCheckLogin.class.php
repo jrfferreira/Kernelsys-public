8199e07a5ca02bae12b58a46b7265d4cab74ff9d
Joao Felix <jr8116@gmail.com>
2011-11-14 14:24:52 -0200

    !Origin Commit

---
+++ classes/app.access/TCheckLogin.class.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Checa e valida o usuario logado
+ *
+ */
+
+class TCheckLogin {
+
+    private $user = NULL;
+
+    public function __construct() {
+
+        try {
+
+            $setId = new TSetControl();
+            $this->id = $setId->getSessionPass('portaCopo');
+                        
+            $this->obsession = new TSession();            
+            $dadosUser = $this->obsession->getValue($this->id);
+            
+            if($dadosUser){
+                $this->user = $dadosUser;
+                if(!$this->user->codigouser or !$this->user->codigopessoa) {
+                    // se não existir, lança um erro
+                    throw new ErrorException("Ouve uma falha ao tentar carregar o perfil do usuario.");
+                }
+            }else {
+            	           	
+                $_SESSION = array();
+                session_destroy();
+       
+                $occupant = $occupant ? '?occupant=' . $occupant : null;
+                throw new ErrorException("Ouve uma falha ao carregar o perfil do usuario logado.");
+                
+                //throw new ErrorException('<script language="JavaScript">location.href="../app.view/'.$occupant.'";</script>');
+                
+            }
+            
+        }catch(Exception $e) {
+            // se não existir, lança um erro
+            echo $e;
+            exit();
+            //new setException("Ouve um problema ao tentar carregar o perfil do usuario. ".$e);
+        }
+
+    }
+
+    public function getUser() {
+        return $this->user;
+    }
+}
+?>
